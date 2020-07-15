ca3351878fc5753ebb42ba7d811f342cb6d7410a
ericagates <ericajoangates@gmail.com>
2020-07-15 11:16:24 -0700

    first 4 user stories

--- src/App.js
+++ src/App.js
@@ -6,15 +6,86 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ["", "", "", "", "", "", "", "", ""],
+      turns: 1
     }
   }
 
+  // Main logic to handle clicks
+  handleLocation = (indexLocation) => {
+    let { turns, squares } = this.state
+    // if index location aready has and X or O
+    if (squares[indexLocation] === 'X' || squares[indexLocation] === 'O') {
+      // send an alert
+      alert("Already selected!")
+    } else {
+      //and if not, then 
+      //if turn is even, update squares with an 'X'
+      if (turns % 2 === 0){
+        squares[indexLocation] = 'X'
+      } else {
+        // turn is odd, update squares with an 'O'
+        squares[indexLocation] = 'O'
+      }
+      // update turn to +1
+      turns += 1
+      // update the square with an X or O
+      this.setState({squares: squares,  turns: turns})
+
+      //then we check for a winner
+      let winner = this.checkWinner()
+      // if winner is and X or O
+      if (winner) {
+        alert( winner + ` is the winner!`)
+      }
+    }  
+  }
+
+  checkWinner = () => {
+    let { squares } = this.state
+    // list the winning index combinations 
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // looping through the winning combinations
+    for (let i = 0; i < winningCombos.length; i++) {
+      // setting a, b, and c to the indexes in the nested array for each winning combo
+      const [a, b, c] = winningCombos[i];
+      // checking if the winning combos all have the same value (either X or O)
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        let winner = squares[a]
+        return winner; // this will return either "X" or "O" 
+      }
+    }
+    // if no winning combos, return null
+    return null;
+
+
+  }
+
   render(){
+    let square = this.state.squares.map((value, index) => {
+      return(
+        <Square
+          value={ value }
+          index={ index }
+          handleLocation={ this.handleLocation}
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id = "board">
+          {square}
+        </div>
       </React.Fragment>
     )
   }
